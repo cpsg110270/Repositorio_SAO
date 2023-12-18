812218010cf0cf96b401ba2a61de202b02390706
cpsg110270 <cpsg110270@hotmail.com>
2023-12-18 16:24:47 -0600

    importar

--- SAO/src/SAO.Web/Pages/ImporExports/Importar.cshtml.cs
+++ SAO/src/SAO.Web/Pages/ImporExports/Importar.cshtml.cs
@@ -1,72 +1,85 @@
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Http;
-//using OfficeOpenXml;
-//using SAO.ImporExports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using OfficeOpenXml;
+using SAO.ImporExports;
 
-//namespace SAO.Web.Pages.ImporExports
-//{
-//    public class ImportarModel : SAOPageModel
-//    {
-//        [BindProperty]
-//        public IFormFile XlsFile { get; set; }
+namespace SAO.Web.Pages.ImporExports
+{
+    public class ImportarModel : SAOPageModel
+    {
+        [BindProperty]
+        public IFormFile XlsFile { get; set; }
 
-//        [TempData]
-//        public string ErrorMessage { get; set; }
+        [TempData]
+        public string ErrorMessage { get; set; }
 
-//        [BindProperty]
-//        public List<TipoCambioCreateDto> ListaTipoCambio { get; set; } = new List<TipoCambioCreateDto>();
+        [BindProperty]
+        public List<ImporExportCreateDto> ListaLiecncias { get; set; } = new List<ImporExportCreateDto>();
 
-//        public IActionResult OnGet()
-//        {
-//            return Page();
-//        }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
 
-//        public IActionResult OnPostImport()
-//        {
-//            ErrorMessage = null;
+        public IActionResult OnPostImport()
+        {
+            ErrorMessage = null;
 
-//            try
-//            {
-//                var stream = XlsFile.OpenReadStream();
+            try
+            {
+                var stream = XlsFile.OpenReadStream();
 
-//                //After save excel file in wwwroot and then
-//                using (ExcelPackage package = new ExcelPackage(stream))
-//                {
-//                    ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
-//                    if (worksheet == null)
-//                    {
-//                        //return or alert message here
-//                        ErrorMessage = "Ha ocurrido un error! No se carg  el archivo";
-//                    }
-//                    else
-//                    {
-//                        //read excel file data and add data in  model.StaffInfoViewModel.StaffList
-//                        var rowCount = worksheet.Dimension.Rows;
-//                        for (int row = 5; row <= rowCount; row++)
-//                        {
-//                            string Fecha = (worksheet.Cells[row, 1].Value ?? string.Empty).ToString().Trim();
-//                            string Valor = (worksheet.Cells[row, 2].Value ?? string.Empty).ToString().Trim();
+                //After save excel file in wwwroot and then
+                using (ExcelPackage package = new ExcelPackage(stream))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                    if (worksheet == null)
+                    {
+                        //return or alert message here
+                        ErrorMessage = "Ha ocurrido un error! No se carga  el archivo";
+                    }
+                    else
+                    {
+                        //read excel file data and add data in  model.StaffInfoViewModel.StaffList
+                        var rowCount = worksheet.Dimension.Rows;
+                        for (int row = 5; row <= rowCount; row++)
+                        {
+                            string Permiso = (worksheet.Cells[row, 1].Value ?? string.Empty).ToString().Trim();
+                            string FechaE  = (worksheet.Cells[row, 2].Value ?? string.Empty).ToString().Trim();
+                            string FechaS  = (worksheet.Cells[row, 3].Value ?? string.Empty).ToString().Trim();
+                            string PesoN   = (worksheet.Cells[row, 4].Value ?? string.Empty).ToString().Trim(); 
+                            string PesoU   = (worksheet.Cells[row, 5].Value ?? string.Empty).ToString().Trim(); 
+                            string CantEnv = (worksheet.Cells[row, 6].Value ?? string.Empty).ToString().Trim(); 
+                            string Factura = (worksheet.Cells[row, 7].Value ?? string.Empty).ToString().Trim(); 
+                             
+                            
+                            ListaLiecncias.Add(new ImporExportCreateDto
+                            {
+                                NoPermiso = Permiso,
+                                FechaEmision = DateTime.Parse(FechaE),
+                                FechaSolicitud = DateTime.Parse(FechaS),
+                                PesoNeto = Double.Parse(PesoN),
+                                PesoUnitario= Double.Parse(PesoU),
+                                CantEnvvase= int.Parse(CantEnv),
+                                NoFactura = Factura,
 
-//                            ListaTipoCambio.Add(new TipoCambioCreateDto
-//                            {
-//                                Fecha = DateTime.Parse(Fecha),
-//                                valor = decimal.Parse(Valor),
-//                            });
-//                        }
+                                 
+                            });
+                        }
 
-//                    }
-//                }
-//            }
-//            catch (Exception ex)
-//            {
-//                ErrorMessage = "Ha ocurrido un error! " + ex.Message;
-//            }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = "Ha ocurrido un error! " + ex.Message;
+            }
 
-//            //return same view and pass view model
-//            return Page();
-//        }
-//    }
-//}
+            //return same view and pass view model
+            return Page();
+        }
+    }
+}
--- SAO/src/SAO.Web/Pages/ImporExports/Importar.js
+++ SAO/src/SAO.Web/Pages/ImporExports/Importar.js
@@ -1,52 +1,52 @@
  
 
-////Loader...
-//$(document).ajaxStart(function () {
-//    abp.ui.setBusy();
-//});
+//Loader...
+$(document).ajaxStart(function () {
+    abp.ui.setBusy();
+});
 
-//$(document).ajaxStop(function () {
-//    abp.ui.clearBusy();
-//});
+$(document).ajaxStop(function () {
+    abp.ui.clearBusy();
+});
 
-//$(document).ajaxError(function () {
-//    abp.ui.clearBusy();
-//});
+$(document).ajaxError(function () {
+    abp.ui.clearBusy();
+});
 
-///* show file value after file select */
-//document.querySelector('.custom-file-input').addEventListener('change', function (e) {
-//    var fileName = document.getElementById("chooseFile").files[0].name;
-//    var nextSibling = e.target.nextElementSibling
-//    nextSibling.innerText = fileName
-//})
+/* show file value after file select */
+document.querySelector('.custom-file-input').addEventListener('change', function (e) {
+    var fileName = document.getElementById("chooseFile").files[0].name;
+    var nextSibling = e.target.nextElementSibling
+    nextSibling.innerText = fileName
+})
 
-//$("#SaveTiposCambiosButton").click(function () {
+$("#SaveTiposCambiosButton").click(function () {
 
-//    $.each($("#tableTiposCambios tbody tr"), function (i, item) {
-//        var fecha = $(this).find('td:eq(0)').html().toString().trim();
-//        var valor = $(this).find('td:eq(1)').html().toString().trim();
-//        abp.ajax({
-//            url: '/api/app/tipoCambio',
-//            type: 'POST',
-//            abpHandleError: false, //DISABLE AUTO ERROR HANDLING
-//            data: JSON.stringify({
-//                fecha: fecha,
-//                valor: valor
-//            }),
-//            success: function (data) {
-//                $(item).find('td:eq(2)').html('<span class="badge badge-success">Éxito</span>');
-//                $(item).find('td:eq(2)').attr('data-original-title', 'Registro guardado con Éxito!');
-//            },
-//            error: function (error) {
-//                if (error.code == 'Exist') {
-//                    $(item).find('td:eq(2)').html('<span class="badge badge-warning">Existe</span>');
-//                    $(item).find('td:eq(2)').attr('data-original-title', error.message);
-//                }
-//                else {
-//                    $(item).find('td:eq(2)').html('<span class="badge badge-danger">Error</span>');
-//                    $(item).find('td:eq(2)').attr('data-original-title', error.message);
-//                }
-//            }
-//        });
-//    });
-//});
+    $.each($("#tableTiposCambios tbody tr"), function (i, item) {
+        var fecha = $(this).find('td:eq(0)').html().toString().trim();
+        var valor = $(this).find('td:eq(1)').html().toString().trim();
+        abp.ajax({
+            url: '/api/app/tipoCambio',
+            type: 'POST',
+            abpHandleError: false, //DISABLE AUTO ERROR HANDLING
+            data: JSON.stringify({
+                fecha: fecha,
+                valor: valor
+            }),
+            success: function (data) {
+                $(item).find('td:eq(2)').html('<span class="badge badge-success">Éxito</span>');
+                $(item).find('td:eq(2)').attr('data-original-title', 'Registro guardado con Éxito!');
+            },
+            error: function (error) {
+                if (error.code == 'Exist') {
+                    $(item).find('td:eq(2)').html('<span class="badge badge-warning">Existe</span>');
+                    $(item).find('td:eq(2)').attr('data-original-title', error.message);
+                }
+                else {
+                    $(item).find('td:eq(2)').html('<span class="badge badge-danger">Error</span>');
+                    $(item).find('td:eq(2)').attr('data-original-title', error.message);
+                }
+            }
+        });
+    });
+});
